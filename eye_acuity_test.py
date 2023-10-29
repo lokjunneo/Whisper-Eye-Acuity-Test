a8478b4299d91f5ee46a27e1423f004277133602
lokjunneo <raizutiamat@gmail.com>
2023-10-30 05:33:12 +0800

    Can demo with new

--- eye_acuity_test.py
+++ eye_acuity_test.py
@@ -2,11 +2,13 @@
 from Flowerchart.flowerchart_node import FlowChartDecisionNode, FlowChartProcessNode, FlowChartNode, FlowChartDelayNode, print_kwargs
 
 from Flowerchart.flowchart_node import *
-from Flowerchart.flowchart_system import *
+from Flowerchart.flowchart_system import FlowchartSystem
 #phonemics
 import eng_to_ipa as p
 
 import random, copy
+
+from functools import partial
 # This class contains the method run_javascript and play_audip which HTMLWindow will implement
 # HTMLWindow will override run_javascript and play_audio with actual implementation
 # This class is used for testing purposes
@@ -21,7 +23,7 @@ class EyeAcuityWrapper:
     def play_audio(self, relative_audio_location):
         print("Audio at '", relative_audio_location, "' will be played")
         
-class EyeAcuityTest():
+class EyeAcuityTest(FlowchartSystem):
     def __init__(self, eye_acuity_wrapper: EyeAcuityWrapper):
         self.in_test = False
         
@@ -52,49 +54,94 @@ class EyeAcuityTest():
         self.eye_acuity_wrapper = eye_acuity_wrapper
         
         self.nodes = {
-            "Default state": FlowChartProcessNode(), #Unsure if working
-            "Await instructions": FlowChartDecisionNode(),
-            "Play initial audio": FlowChartProcessNode(),
-            "Wait for instructions to finish": FlowChartDelayNode(),
-            "Display 6/120 letter": FlowChartProcessNode(),
-            "Alert PSA": FlowChartDecisionNode(),
-            "Validate 6/120": FlowChartDecisionNode(),
-            "Display 6/60 and 6/45": FlowChartProcessNode(),
-            "Validate 6/60 and 6/45": FlowChartDecisionNode()
+            "Default state": ProcessNode(), #Unsure if working
+            "Listen for start": IONode(),
+            "Check for start": DecisionNode(),
+            "Play initial audio": ProcessNode(),
+            "Wait for initial audio finish": DelayNode(),
+            "Display 6/120 letter": ProcessNode(),
+            "Alert PSA": DecisionNode(),
+            "Read 6/120": IONode(),
+            "Validate 6/120": DecisionNode(),
+            "Generic error": ProcessNode(), # Don't define next_node; it is dynamic
+            "Generic read": DelayNode(), #Cloneable, to wait for text input
+            "Generic validate": DecisionNode(), #Cloneable, to validate input
+            "Generic clear screen": ProcessNode(), #Cloneable, to remove letters
+            "Display 6/60 and 6/45": ProcessNode(),
         }
         
+        def check_for_start(self):
+            text = self.context["process_text"]
+            text = text.lower()
+            if "start" in text or "begin" in text:
+                return True
+            return False
+        
+        def generic_error(this: ProcessNode):
+            self.eye_acuity_wrapper.play_audio("/tts_recordings/please_try_again.mp3")
+            this.next_node = this.previous_node.previous_node
+            
+        def assign_generic_rv(curr_node, true_node):
+            '''
+            Generic block to wait for input, then validate input, then clear screen if letters are read correctly
+            '''
+            my_gr = copy.copy(self.gr)
+            my_gv = copy.copy(self.gv)
+            my_gcs = copy.copy(self.gcs)
+            curr_node.next_node = my_gr
+            my_gr.next_node = my_gv
+            my_gv.callback = partial(self.check_characters, my_gv)
+            my_gv.false_node = self.ge
+            my_gv.true_node = my_gcs
+            my_gcs.next_node = true_node
+        
         self.current_node = self.nodes["Default state"]
-        self.nodes["Default state"].set_callbacks( [ (self.display_instructions, {}), 
-                                                    (print_kwargs, {"output": "Please enter start or begin."}) 
-                                                    ] )
-        self.nodes["Default state"].next_node = self.nodes["Await instructions"]
+        self.ge = self.nodes["Generic error"]
+        self.ge.callback = partial(generic_error, self.ge)
         
-        self.nodes["Await instructions"].set_condition_callbacks( [ (self.start_instructions, {}) ] )
-        self.nodes["Await instructions"].false_node = self.nodes["Default state"]
-        self.nodes["Await instructions"].next_node = self.nodes["Play initial audio"]
+        # Cloneable to wait for user input
+        self.gr = self.nodes["Generic read"]
+        self.gr.set_signal_key("process_text")
         
-        self.nodes["Play initial audio"].set_callbacks([
-            (self.play_audio, {"audio_file_path": "/tts_recordings/stand_4_meters_away.mp3"})
-        ])
-        self.nodes["Play initial audio"].next_node = self.nodes["Wait for instructions to finish"]
+        self.gv = self.nodes["Generic validate"]
         
-        # Delay node to make sure instructions finish playing
-        self.nodes["Wait for instructions to finish"].next_node = self.nodes["Display 6/120 letter"]
+        self.gcs = self.nodes["Generic clear screen"]
+        self.gcs.callback = self.remove_letters
         
-        # Display 6/120 letter
-        self.nodes["Display 6/120 letter"].set_callbacks( [(self.display_characters, 
-                                                            {"c_array":[{"num_of_characters": 1,"vam_size": "6/120"}]
-                                                             }),
-                                                        ] )
-        self.nodes["Display 6/120 letter"].next_node = self.nodes["Validate 6/120"]
+        self.nodes["Default state"].callback = partial(self.display_instructions)
+        self.nodes["Default state"].next_node = self.nodes["Listen for start"]
+        #self.nodes["Listen for start"]: IONode
+        self.nodes["Listen for start"].set_signal_key("process_text")
+        self.nodes["Listen for start"].next_node = self.nodes["Check for start"]
         
-        # Validate 6/120
-        self.nodes["Validate 6/120"].set_condition_callbacks( [ (self.check_characters, {})])
-        self.nodes["Validate 6/120"].set_false_callbacks( [ 
-                                                           (self.please_try_again,{}),
-                                                           (print_kwargs, {"output": "ALERT PSA"})])
+        cfs = self.nodes["Check for start"]
+        cfs.callback = partial(check_for_start, cfs)
+        cfs.false_node = self.nodes["Default state"]
+        cfs.true_node = self.nodes["Play initial audio"]
         
-        def remove_letters(**kwargs):
+        pia = self.nodes["Play initial audio"]
+        pia.callback = partial(self.eye_acuity_wrapper.play_audio, "/tts_recordings/stand_4_meters_away.mp3")
+        pia.next_node = self.nodes["Wait for initial audio finish"]
+        
+        wiaf = self.nodes["Wait for initial audio finish"]
+        wiaf.set_signal_key("audio_done")
+        wiaf.next_node = self.nodes["Display 6/120 letter"]
+        
+        d6120 = self.nodes["Display 6/120 letter"]
+        d6120.callback = partial(self.display_characters, (1, "6/120"))
+        assign_generic_rv(d6120, self.nodes["Display 6/60 and 6/45"])
+        
+        d660645 = self.nodes["Display 6/60 and 6/45"]
+        d660645.callback = partial(self.display_characters, (1, "6/60"), (1, "6/45"))
+        assign_generic_rv(d660645, d660645)
+        
+    '''
+    Methods used for acuity test display
+    '''
+    def display_instructions(self, **kwargs):
+        # If kwargs has no parameter "text", text will have a default value of "Insert text"
+        text = "Insert text" if ("text" not in kwargs) else kwargs["text"]
+    def remove_letters(self):
             javascript = """
             var lettersContainer = document.querySelector(".letters");
 
@@ -104,56 +151,15 @@ class EyeAcuityTest():
             }
             """
             self.eye_acuity_wrapper.run_javascript(javascript)
-            
-        self.nodes["Validate 6/120"].set_true_callbacks([ (remove_letters,{})
-                                                          ])
-        self.nodes["Validate 6/120"].next_node = self.nodes["Display 6/60 and 6/45"]
-        
-        # Display 6/60 and 6/45
-        self.nodes["Display 6/60 and 6/45"].set_callbacks( [(self.display_characters, 
-                                                            {"c_array":[
-                                                                {"num_of_characters": 1,"vam_size": "6/60"},
-                                                                {"num_of_characters": 1, "vam_size": "6/45"}
-                                                            ]}),
-                                                           (print_kwargs, {"output": "Display 6/120 letter"}) ] )
-        self.nodes["Display 6/60 and 6/45"].next_node = self.nodes["Validate 6/120"]
-        
-        # Validate 6/60 and 6/45
-        self.nodes["Validate 6/60 and 6/45"] = copy.copy(self.nodes["Validate 6/120"])
-        self.nodes["Validate 6/60 and 6/45"].next_node = self.nodes["Display 6/60 and 6/45"]
-        
-        #self.nodes["Validate 6/60"].next_node = self.nodes["Play initial audio"]
-        #self.nodes["Validate 6/120"].false_node = self.nodes[""]
-    
-    
-    # process nodes
-    def execute_process(self):
-        self.current_node: FlowChartProcessNode = self.current_node.execute()
-    
-    # decision nodes
-    def execute_decision(self, input):
-        self.current_node: FlowChartDecisionNode = self.current_node.execute(input)
-        
-    # delay nodes
-    def execute_delay(self):
-        self.current_node: FlowChartDelayNode = self.current_node.execute()
-    '''
-    Methods used for acuity test display
-    '''
-    def display_instructions(self, **kwargs):
-        # If kwargs has no parameter "text", text will have a default value of "Insert text"
-        text = "Insert text" if ("text" not in kwargs) else kwargs["text"]
-        
-    def display_characters(self, **kwargs):
+    def display_characters(self, *chara_tuples):
         '''
-        Method only supports 1 size, should be revamped to support
-        <!> Multiple characters of different sizes
+        (num_of_characters, vam_size)
         <!> Random positioning of these characters, no overlap
         '''
-        for i in kwargs["c_array"]:
-            self.characters_displayed = {}
-            num_of_characters = i["num_of_characters"]
-            vam_size = i["vam_size"]
+        self.characters_displayed = {}
+        for i in chara_tuples:
+            num_of_characters = i[0]
+            vam_size = i[1]
             font_size = self.vam_to_font_size[vam_size]
             # <!> Generate characters, based on num_of_characters
             #num_of_characters = kwargs["num_of_characters"]
@@ -185,21 +191,31 @@ class EyeAcuityTest():
             # <!> Call run_javascript method of eye_acuity_wrapper
             self.eye_acuity_wrapper.run_javascript(javascript)
         
-    def check_characters(self, **kwargs):
+    def check_characters(self, node):
+        # <!> Order currently not maintained
+        context = node.context
+        print(node)
+        print(node.next_node)
+        print("Context is ", context)
         self.characters_score = {}
-        user_input = kwargs["conditional_var"]
-        user_input = p.convert(user_input).lower()
+        user_input = context["process_text"]
+        o_user_input = user_input
+        user_input = user_input.lower()
         user_input = user_input.replace("-"," ")
         
         # Bandaid for issue: "Letters stuck together may be considered as word, hence ipa conversion fails"
         total_char_length = 0
+        # Calculate total length of characters
         for vam_size, characters in self.characters_displayed.items():
             total_char_length+=len(characters)
-        if len(user_input) == total_char_length:
-            user_input = user_input.split("")
+        if len(user_input) <= total_char_length:
+            user_input = list(user_input)
         else:
             user_input = user_input.split(" ")
-        
+        ipa_user_input = []
+        for i in user_input:
+            ipa_user_input.append(p.convert(i))
+        om_user_input = user_input
         # Tabulate score for each characters
         for vam_size,characters in self.characters_displayed.items():
             self.characters_score[vam_size] = 0
@@ -207,14 +223,20 @@ class EyeAcuityTest():
                 # Get the ipa(s) of the displayed character
                 ipas = self.character_ipa_map[cd]
                 for ipa in ipas:
-                    if ipa in user_input:
+                    if ipa in ipa_user_input:
                         # If user said character, score increases
                         self.characters_score[vam_size] += 1
-                        user_input.remove(ipa)
+                        ipa_user_input.remove(ipa)
                         break
         
         for vam_size, score in self.characters_score.items():
             if len(self.characters_displayed[vam_size]) > score:
+                print("<Check characters> False")
+                print(self.characters_displayed)
+                print(self.characters_score)
+                
+                print(o_user_input)
+                print(om_user_input)
                 return False
         return True
         
@@ -228,6 +250,8 @@ class EyeAcuityTest():
         
         
     def start_instructions(self, **kwargs):
+        pass
+        '''
         user_input = kwargs["conditional_var"]
         #html_window = kwargs["html_window"]
         if "start" in user_input.lower() or "begin" in user_input.lower():
@@ -238,6 +262,7 @@ class EyeAcuityTest():
                 print("Already〖 STARTED 〗")
             return True
         return False
+        '''
 
     def play_audio(self, **kwargs):
         audio_file_path = kwargs["audio_file_path"]
@@ -250,12 +275,12 @@ if __name__ == "__main__":
     #sys.exit(app.exec_())
     jr = EyeAcuityWrapper()
     eat = EyeAcuityTest(jr)
-    eat.current_node = eat.current_node.execute()
+    eat.run()
     #eat.old_node = eat.current_node
     #while eat.old_node == eat.current_node:
     while eat.current_node is not None:
-        if isinstance(eat.current_node,FlowChartDecisionNode):
-            eat.current_node = eat.current_node.execute(input("Enter input: "))
-        elif isinstance(eat.current_node,FlowChartDelayNode):
-            print("<Delay node> Pretending delay has passed")
-            eat.current_node = eat.current_node.execute()
+        print("Current node:", eat.current_node)
+        #s_key = input("Enter signal key: ")
+        s_data = input("Enter signal data: ")
+        eat.signal("process_text", s_data)
+        eat.signal("audio_done", s_data)
